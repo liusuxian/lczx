7fc33838c69e93543f4a6e660f8d103622c8f0ef
liusuxian <382185882@qq.com>
2022-03-24 01:50:22 +0800

    update

--- manifest/sql/lczx.sql
+++ manifest/sql/lczx.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 22/03/2022 23:01:58
+ Date: 23/03/2022 23:57:30
 */
 
 SET NAMES utf8mb4;
@@ -24,6 +24,9 @@ DROP TABLE IF EXISTS `dept`;
 CREATE TABLE `dept` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '部门ID',
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '部门名称',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
+  `deleted_at` datetime DEFAULT NULL COMMENT '软删除时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -31,8 +34,8 @@ CREATE TABLE `dept` (
 -- Records of dept
 -- ----------------------------
 BEGIN;
-INSERT INTO `dept` (`id`, `name`) VALUES (1, '市场运营部');
-INSERT INTO `dept` (`id`, `name`) VALUES (2, '技术支持部');
+INSERT INTO `dept` (`id`, `name`, `create_at`, `update_at`, `deleted_at`) VALUES (1, '市场运营部', '2022-03-23 23:39:59', '2022-03-23 23:39:59', NULL);
+INSERT INTO `dept` (`id`, `name`, `create_at`, `update_at`, `deleted_at`) VALUES (2, '技术支持部', '2022-03-23 23:40:47', '2022-03-23 23:40:47', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -72,7 +75,7 @@ DROP TABLE IF EXISTS `user_online`;
 CREATE TABLE `user_online` (
   `id` int unsigned NOT NULL COMMENT '用户ID',
   `last_time` datetime DEFAULT NULL COMMENT '最后一次登录时间',
-  `last_ip` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '最后一次登录ip',
+  `last_ip` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '最后一次登录ip',
   `visit_count` int unsigned DEFAULT NULL COMMENT '访问次数',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
