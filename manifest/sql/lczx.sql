0bca6950b479373e17255d7685dde420685d3d68
liusuxian <382185882@qq.com>
2022-03-25 15:14:27 +0800

    update

--- manifest/sql/lczx.sql
+++ manifest/sql/lczx.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 24/03/2022 22:51:33
+ Date: 25/03/2022 15:13:11
 */
 
 SET NAMES utf8mb4;
@@ -23,7 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `dept`;
 CREATE TABLE `dept` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '部门ID',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '部门名称',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '部门名称',
   `create_at` datetime DEFAULT NULL COMMENT '创建时间',
   `update_at` datetime DEFAULT NULL COMMENT '更新时间',
   `deleted_at` datetime DEFAULT NULL COMMENT '软删除时间',
@@ -39,6 +39,30 @@ INSERT INTO `dept` (`id`, `name`, `create_at`, `update_at`, `deleted_at`) VALUES
 COMMIT;
 
 -- ----------------------------
+-- Table structure for login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `login_log`;
+CREATE TABLE `login_log` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '访问ID',
+  `passport` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录账号',
+  `ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录IP地址',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录地点',
+  `browser` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '浏览器类型',
+  `os` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '操作系统',
+  `status` tinyint unsigned DEFAULT NULL COMMENT '登录状态 0:失败 1:成功',
+  `msg` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '提示消息',
+  `time` datetime DEFAULT NULL COMMENT '登录时间',
+  `module` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录模块',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of login_log
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -48,12 +72,12 @@ CREATE TABLE `user` (
   `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
   `realname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '姓名',
   `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
-  `gender` int unsigned NOT NULL COMMENT '性别 1: 男 2: 女',
+  `gender` tinyint unsigned NOT NULL COMMENT '性别 1: 男 2: 女',
   `avatar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '头像地址',
   `mobile` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '手机号',
   `dept_id` int unsigned NOT NULL COMMENT '部门ID',
   `role_id` int unsigned NOT NULL COMMENT '角色ID 0: 默认普通用户 1000: 超级管理员 900: 普通管理员',
-  `status` int unsigned NOT NULL COMMENT '状态 0:启用 1:禁用',
+  `status` tinyint unsigned NOT NULL COMMENT '状态 0:禁用 1:启用',
   `create_at` datetime DEFAULT NULL COMMENT '创建时间',
   `update_at` datetime DEFAULT NULL COMMENT '更新时间',
   `deleted_at` datetime DEFAULT NULL COMMENT '软删除时间',
@@ -69,10 +93,10 @@ INSERT INTO `user` (`id`, `passport`, `password`, `realname`, `nickname`, `gende
 COMMIT;
 
 -- ----------------------------
--- Table structure for user_online
+-- Table structure for user_ext
 -- ----------------------------
-DROP TABLE IF EXISTS `user_online`;
-CREATE TABLE `user_online` (
+DROP TABLE IF EXISTS `user_ext`;
+CREATE TABLE `user_ext` (
   `id` int unsigned NOT NULL COMMENT '用户ID',
   `last_time` datetime DEFAULT NULL COMMENT '最后一次登录时间',
   `last_ip` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '最后一次登录ip',
@@ -81,7 +105,7 @@ CREATE TABLE `user_online` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
--- Records of user_online
+-- Records of user_ext
 -- ----------------------------
 BEGIN;
 COMMIT;
