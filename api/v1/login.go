a9bd666e6a69e913a223e9cb9d7af02b53c1387c
liusuxian <382185882@qq.com>
2022-03-28 15:52:36 +0800

    update

--- api/v1/login.go
+++ api/v1/login.go
@@ -9,7 +9,7 @@ type LoginReq struct {
 	Passport   string `json:"passport" v:"required|passport#账号不能为空|账号以字母开头，只能包含字母、数字和下划线，长度在6~18之间" dc:"账号"` // 账号
 	Password   string `json:"password" v:"required|password#密码不能为空|密码为任意可见字符，长度在6~18之间" dc:"密码"`             // 密码
 	VerifyCode string `json:"verifyCode" v:"required|regex:^[a-zA-Z0-9]*$#验证码不能为空|验证码格式错误"`                  // 验证码
-	VerifyKey  string `json:"verifyKey" v:"required"`
+	VerifyKey  string `json:"verifyKey" v:"required|regex:^[a-zA-Z0-9]*$#验证码key不能为空|验证码key格式错误"`             // 验证码key
 }
 type LoginRes struct {
 	Token string `json:"token" dc:"token"` // token
