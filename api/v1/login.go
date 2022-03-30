a636bde0c397841040727fda808b50811fd14699
liusuxian <382185882@qq.com>
2022-03-30 14:01:17 +0800

    update

--- api/v1/login.go
+++ api/v1/login.go
@@ -6,10 +6,10 @@ import (
 
 type LoginReq struct {
 	g.Meta     `path:"/login" tags:"Login" method:"post" summary:"You first login api"`
-	Passport   string `json:"passport" v:"required|passport#账号不能为空|账号以字母开头，只能包含字母、数字和下划线，长度在6~18之间" dc:"账号"`         // 账号
-	Password   string `json:"password" v:"required|password#密码不能为空|密码为任意可见字符，长度在6~18之间" dc:"密码"`                     // 密码
-	VerifyKey  string `json:"verifyKey" v:"required|regex:^[a-zA-Z0-9]*$#验证码key不能为空|验证码key格式错误"`                     // 验证码key
-	VerifyCode string `json:"verifyCode" v:"required|max-length:4|regex:^[a-zA-Z0-9]*$#验证码不能为空|验证码不能超过4个字符|验证码格式错误"` // 验证码
+	Passport   string `json:"passport" v:"required|passport#账号不能为空|账号以字母开头，只能包含字母、数字和下划线，长度在6~18之间" dc:"账号"`           // 账号
+	Password   string `json:"password" v:"required|password#密码不能为空|密码为任意可见字符，长度在6~18之间" dc:"密码"`                       // 密码
+	VerifyKey  string `json:"verifyKey" v:"required|size:20|regex:^[a-zA-Z0-9]*$#验证码key不能为空|验证码key长度必须为20|验证码key格式错误"` // 验证码key
+	VerifyCode string `json:"verifyCode" v:"required|size:4|regex:^[a-zA-Z0-9]*$#验证码不能为空|验证码长度必须为4|验证码格式错误"`           // 验证码
 }
 type LoginRes struct {
 	Token string `json:"token" dc:"token"` // token
