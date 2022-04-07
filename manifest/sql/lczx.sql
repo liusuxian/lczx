8c5c71f284a3e308adc6dcd8956a4a7a75aac5c8
liusuxian <382185882@qq.com>
2022-04-07 13:47:45 +0800

    update

--- manifest/sql/lczx.sql
+++ manifest/sql/lczx.sql
@@ -11,26 +11,24 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 07/04/2022 10:24:31
+ Date: 07/04/2022 13:45:52
 */
 
 SET NAMES utf8mb4;
-SET
-FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for casbin_rule
 -- ----------------------------
 DROP TABLE IF EXISTS `casbin_rule`;
-CREATE TABLE `casbin_rule`
-(
-    `p_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    `v0`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `v1`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `v2`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `v3`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `v4`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `v5`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+CREATE TABLE `casbin_rule` (
+  `p_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `v0` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v4` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
@@ -43,18 +41,17 @@ COMMIT;
 -- Table structure for dept
 -- ----------------------------
 DROP TABLE IF EXISTS `dept`;
-CREATE TABLE `dept`
-(
-    `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '部门ID',
-    `parent_id`  bigint unsigned DEFAULT NULL COMMENT '父部门ID',
-    `name`       varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '部门名称',
-    `status`     tinyint unsigned DEFAULT NULL COMMENT '部门状态 0:停用 1:正常',
-    `created_by` bigint unsigned DEFAULT NULL COMMENT '创建人',
-    `updated_by` bigint unsigned DEFAULT NULL COMMENT '修改人',
-    `create_at`  datetime                                                     DEFAULT NULL COMMENT '创建时间',
-    `update_at`  datetime                                                     DEFAULT NULL COMMENT '更新时间',
-    `deleted_at` datetime                                                     DEFAULT NULL COMMENT '软删除时间',
-    PRIMARY KEY (`id`)
+CREATE TABLE `dept` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '部门ID',
+  `parent_id` bigint unsigned DEFAULT NULL COMMENT '父部门ID',
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '部门名称',
+  `status` tinyint unsigned DEFAULT NULL COMMENT '部门状态 0:停用 1:正常',
+  `created_by` bigint unsigned DEFAULT NULL COMMENT '创建人',
+  `updated_by` bigint unsigned DEFAULT NULL COMMENT '修改人',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
+  `deleted_at` datetime DEFAULT NULL COMMENT '软删除时间',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
@@ -67,19 +64,18 @@ COMMIT;
 -- Table structure for login_log
 -- ----------------------------
 DROP TABLE IF EXISTS `login_log`;
-CREATE TABLE `login_log`
-(
-    `id`       bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '访问ID',
-    `passport` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '登录账号',
-    `ip`       varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '登录IP地址',
-    `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录地点',
-    `browser`  varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '浏览器类型',
-    `os`       varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '操作系统',
-    `status`   tinyint unsigned DEFAULT NULL COMMENT '登录状态 0:失败 1:成功',
-    `msg`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '提示消息',
-    `time`     datetime                                                      DEFAULT NULL COMMENT '登录时间',
-    `module`   varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '登录模块',
-    PRIMARY KEY (`id`)
+CREATE TABLE `login_log` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '访问ID',
+  `passport` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录账号',
+  `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录IP地址',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录地点',
+  `browser` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '浏览器类型',
+  `os` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '操作系统',
+  `status` tinyint unsigned DEFAULT NULL COMMENT '登录状态 0:失败 1:成功',
+  `msg` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '提示消息',
+  `time` datetime DEFAULT NULL COMMENT '登录时间',
+  `module` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '登录模块',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
@@ -92,17 +88,16 @@ COMMIT;
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role`
-(
-    `id`         int unsigned NOT NULL AUTO_INCREMENT COMMENT '角色ID',
-    `status`     tinyint unsigned NOT NULL COMMENT '状态 0:停用 1:正常',
-    `name`       varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '角色名称',
-    `remark`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '备注',
-    `data_scope` tinyint unsigned NOT NULL COMMENT '数据范围 1:全部数据权限 2:自定义数据权限 3:本部门数据权限 4:本部门及以下数据权限',
-    `create_at`  datetime DEFAULT NULL COMMENT '创建时间',
-    `update_at`  datetime DEFAULT NULL COMMENT '更新时间',
-    PRIMARY KEY (`id`),
-    KEY          `status_index` (`status`)
+CREATE TABLE `role` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `status` tinyint unsigned NOT NULL COMMENT '状态 0:停用 1:正常',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '角色名称',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '备注',
+  `data_scope` tinyint unsigned NOT NULL COMMENT '数据范围 1:全部数据权限 2:自定义数据权限 3:本部门数据权限 4:本部门及以下数据权限',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `status_index` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
@@ -115,60 +110,54 @@ COMMIT;
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`
-(
-    `id`              bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-    `passport`        varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
-    `password`        varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
-    `salt`            char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci    NOT NULL COMMENT '加密盐',
-    `realname`        varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '姓名',
-    `nickname`        varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '昵称',
-    `dept_id`         int unsigned NOT NULL COMMENT '部门ID',
-    `gender`          tinyint unsigned NOT NULL COMMENT '性别 1: 男 2: 女',
-    `status`          tinyint unsigned NOT NULL COMMENT '状态 0:禁用 1:启用',
-    `avatar`          text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '头像地址',
-    `mobile`          varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '手机号',
-    `email`           varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户邮箱',
-    `remark`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
-    `last_login_ip`   varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  DEFAULT NULL COMMENT '最后登录ip',
-    `last_login_time` datetime                                                      DEFAULT NULL COMMENT '最后登录时间',
-    `create_at`       datetime                                                      DEFAULT NULL COMMENT '创建时间',
-    `update_at`       datetime                                                      DEFAULT NULL COMMENT '更新时间',
-    `deleted_at`      datetime                                                      DEFAULT NULL COMMENT '软删除时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `passport_index` (`passport`),
-    UNIQUE KEY `mobile_index` (`mobile`),
-    KEY               `realname_index` (`realname`),
-    KEY               `nickname_index` (`nickname`)
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `passport` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `salt` char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '加密盐',
+  `realname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '姓名',
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
+  `dept_id` int unsigned NOT NULL COMMENT '部门ID',
+  `gender` tinyint unsigned NOT NULL COMMENT '性别 1: 男 2: 女',
+  `status` tinyint unsigned NOT NULL COMMENT '状态 0:禁用 1:启用',
+  `avatar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT '头像地址',
+  `mobile` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '手机号',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户邮箱',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
+  `last_login_ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '最后登录ip',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+  `create_at` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime DEFAULT NULL COMMENT '更新时间',
+  `deleted_at` datetime DEFAULT NULL COMMENT '软删除时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `passport_index` (`passport`),
+  UNIQUE KEY `mobile_index` (`mobile`),
+  KEY `realname_index` (`realname`),
+  KEY `nickname_index` (`nickname`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` (`id`, `passport`, `password`, `salt`, `realname`, `nickname`, `dept_id`, `gender`, `status`,
-                    `avatar`, `mobile`, `email`, `remark`, `last_login_ip`, `last_login_time`, `create_at`, `update_at`,
-                    `deleted_at`)
-VALUES (1, 'sadmin', 'b0167dd859b8a70478da36238b0b3e05', 'CqDQa4THP1', '超级管理员', '超级管理员', 0, 1, 1, NULL, NULL, NULL,
-        NULL, '127.0.0.1', '2022-03-28 16:28:53', '2022-03-25 17:41:40', '2022-03-25 17:41:40', NULL);
+INSERT INTO `user` (`id`, `passport`, `password`, `salt`, `realname`, `nickname`, `dept_id`, `gender`, `status`, `avatar`, `mobile`, `email`, `remark`, `last_login_ip`, `last_login_time`, `create_at`, `update_at`, `deleted_at`) VALUES (1, 'sadmin', 'b0167dd859b8a70478da36238b0b3e05', 'CqDQa4THP1', '超级管理员', '超级管理员', 0, 1, 1, NULL, NULL, NULL, NULL, '127.0.0.1', '2022-03-28 16:28:53', '2022-03-25 17:41:40', '2022-03-25 17:41:40', NULL);
 COMMIT;
 
 -- ----------------------------
 -- Table structure for user_online
 -- ----------------------------
 DROP TABLE IF EXISTS `user_online`;
-CREATE TABLE `user_online`
-(
-    `id`       bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '访问ID',
-    `uuid`     varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '用户标识',
-    `token`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户token',
-    `passport` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '登录账号',
-    `browser`  varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '浏览器类型',
-    `os`       varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '操作系统',
-    `ip`       varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '登录ip',
-    `time`     datetime DEFAULT NULL COMMENT '登录时间',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `token_index` (`token`)
+CREATE TABLE `user_online` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '访问ID',
+  `uuid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户标识',
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '用户token',
+  `passport` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '登录账号',
+  `browser` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '浏览器类型',
+  `os` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '操作系统',
+  `ip` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '登录ip',
+  `time` datetime DEFAULT NULL COMMENT '登录时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token_index` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
@@ -177,5 +166,4 @@ CREATE TABLE `user_online`
 BEGIN;
 COMMIT;
 
-SET
-FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
