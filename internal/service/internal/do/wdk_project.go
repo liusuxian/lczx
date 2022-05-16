1ec413b96da7256a1cdf654793548ab57da8498b
liusuxian <382185882@qq.com>
2022-05-16 14:59:12 +0800

    update

--- internal/service/internal/do/wdk_project.go
+++ internal/service/internal/do/wdk_project.go
@@ -19,6 +19,9 @@ type WdkProject struct {
 	Step             interface{} // 项目阶段 0:未开始 1:合同签约 2:项目启动会 3:服务中 4:合同结束 5:复盘
 	FileUploadStatus interface{} // 项目文件上传状态 0:未传完 1:已传完
 	BusinessType     interface{} // 业务类型 0:物业 1:专项 2:全过程
+	BusinessForms    interface{} // 业态 0:住宅-综合性住宅 1:住宅-高层住宅 2:住宅-多层住宅 3:住宅-联排别墅 4:住宅-独立式住宅 5:非住宅-办公用房 6:非住宅-场馆（体育馆、博物馆、机场） 7:非住宅-综合类 8:非住宅-学校 9:非住宅-工业园 10:非住宅-医院 11:非住宅-商场 12:非住宅-商铺 13:非住宅-车位 14:非住宅-酒店式公寓 15:非住宅-仓储 16:非住宅-经营用房 17:非住宅-物业用房 18:非住宅-配套用房 19:非住宅-车库 20:非住宅-会所 21:非住宅-临停车位 22:非住宅-非机动车位 23:非住宅-社会福利院 24:非住宅-旅游景区
+	ContractStatus   interface{} // 签约状态 0:新签 1:续签
+	ContractSum      interface{} // 合同金额
 	DeepCulture      interface{} // 是否为深耕 0:否 1:是
 	Status           interface{} // 服务状态 0:服务中 1:暂停 2:提前终止 3:正常结束
 	EntrustCompany   interface{} // 委托方公司
