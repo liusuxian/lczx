c0eb85bf8a71b37322a2133541a8079c4657e8b5
liusuxian <382185882@qq.com>
2022-06-15 11:04:11 +0800

    update

--- internal/controller/download.go
+++ internal/controller/download.go
@@ -16,6 +16,8 @@ type cDownload struct{}
 // WdkProjectFile 下载文档库项目信息文件
 func (c *cDownload) WdkProjectFile(ctx context.Context, req *v1.DownloadWdkProjectFileReq) (res *v1.DownloadWdkProjectFileRes, err error) {
 	g.RequestFromCtx(ctx).Response.ServeFileDownload(req.FilePath)
-	_ = gfile.Remove(req.FilePath)
+	if gfile.Exists(req.FilePath) {
+		_ = gfile.Remove(req.FilePath)
+	}
 	return
 }
