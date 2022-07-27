c5f4976f5102f27b4896bbb6deb2f17d93fd8fec
liusuxian <382185882@qq.com>
2022-07-27 12:27:52 +0800

    update

--- internal/logic/aliyun_oss/download.go
+++ internal/logic/aliyun_oss/download.go
@@ -1,14 +1,9 @@
 package aliyun_oss
 
 import (
-	"context"
-	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/text/gstr"
-	"io"
 	"lczx/utility/crypto"
-	"net/url"
 )
 
 // AuthorizationDownload 授权给第三方下载
@@ -39,52 +34,5 @@ func (s *sAliyunOSS) AuthorizationDownload(filePath string) (fileUrl string, err
 	}
 	// 授权访问
 	fileUrl, err = bucket.SignURL(filePath, oss.HTTPGet, 60)
-	return
-}
-
-// BlobDownload 文件流下载
-func (s *sAliyunOSS) BlobDownload(ctx context.Context, filePath string) (body io.ReadCloser, err error) {
-	// 解密 accessKeyID
-	var accessKeyID []byte
-	accessKeyID, err = crypto.AesDecrypt(s.accessKeyID)
-	if err != nil {
-		return
-	}
-	// 解密 accessKeySecret
-	var accessKeySecret []byte
-	accessKeySecret, err = crypto.AesDecrypt(s.accessKeySecret)
-	if err != nil {
-		return
-	}
-	// 连接OSS
-	var client *oss.Client
-	var endpoint string
-	if upCfg.env == 1 {
-		endpoint = s.endpointInternal
-	} else {
-		endpoint = s.endpointAccelerate
-	}
-	client, err = oss.New(endpoint, gstr.TrimAll(string(accessKeyID)), gstr.TrimAll(string(accessKeySecret)))
-	if err != nil {
-		return
-	}
-	// 获取存储空间
-	var bucket *oss.Bucket
-	bucket, err = client.Bucket(s.bucket)
-	if err != nil {
-		return
-	}
-	// 下载文件
-	body, err = bucket.GetObject(filePath)
-	if err != nil {
-		return
-	}
-	// 获取文件名
-	strList := gstr.Split(filePath, "/")
-	filename := strList[len(strList)-1]
-	// 设置响应头
-	request := g.RequestFromCtx(ctx)
-	request.Response.Header().Set("Content-Disposition", fmt.Sprintf(`attachment;filename=%s`, url.QueryEscape(filename)))
-	request.Response.Header().Set("Access-Control-Expose-Headers", "content-disposition")
 	return
 }
--- internal/service/aliyun_oss.go
+++ internal/service/aliyun_oss.go
@@ -6,7 +6,6 @@ package service
 
 import (
 	"context"
-	"io"
 	"lczx/internal/model"
 
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -14,7 +13,6 @@ import (
 
 type IAliyunOSS interface {
 	AuthorizationDownload(filePath string) (fileUrl string, err error)
-	BlobDownload(ctx context.Context, filePath string) (body io.ReadCloser, err error)
 	UploadImg(file *ghttp.UploadFile, dirPath string) (fileInfo *model.UploadFileInfo, err error)
 	UploadFile(file *ghttp.UploadFile, dirPath string) (fileInfo *model.UploadFileInfo, err error)
 	UploadImgs(files []*ghttp.UploadFile, dirPath string) (fileInfos []*model.UploadFileInfo, err error)
